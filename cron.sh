b09e1131d8543bcd6e695761bcb108a4acdacea3
Justal Kevin <justal.kevin@gmail.com>
2023-11-21 07:02:12 +0100

    [REFACTORING] Adding start NPM

--- cron.sh
+++ cron.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cd /home/admin/code/POC-REFRESH/
 npm run start
 npm run sub-install
-npm run sub-start
+
+pm2 describe NFC > /dev/null
+RUNNING=$?
+
+if [ "${RUNNING}" -ne 0 ]; then
+  pm2 start npm --name "NFC" -- start --prefix POC-NFC-master
+fi;
